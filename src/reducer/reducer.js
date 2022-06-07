9152c97546c00afecacdeb50d1b057d34171b8b8
diegoalexanderdiazhernandez <diegodiazh1994@gmail.com>
2022-06-07 09:13:17 -0500

    feature: change url js

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -3,7 +3,7 @@ import {POST_LOGIN, REFRESH} from '../actions/types';
 const initialState = {
   user: {success: false, user: {}, message: ''},
   refresh: false,
-  baseUrl: 'http://44.202.24.246:8080',
+  baseUrl: 'http://35.171.19.122:8080',
 };
 
 const reducer = (state = initialState, action) => {
